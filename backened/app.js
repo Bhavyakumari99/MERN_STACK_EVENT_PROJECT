95645e6d4eb2d0719ead7cd037ca749ff954dd69
Bhavyakumari99 <kumaribhavya425@gmail.com>
2025-08-10 22:24:58 +0530

    Update app.js

--- backened/app.js
+++ backened/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { dbconnection } from "./database/dbConnection.js";
+import { dbconnection } from "./database/dbconnection.js";
 import dotenv from "dotenv";
 import messageRouter from "./router/messageRouter.js";
 import cors from "cors";
