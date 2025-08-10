a70cdbdaa92c4cde80637f0670f7b32c918caf02
Bhavya kumari <kumaribhavya425@gmail.com>
2025-08-10 17:46:00 +0530

    first commit

---
+++ backened/database/dbconnection.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export const dbconnection = () =>{
+ mongoose
+    .connect(process.env.MONGO_URI, {dbName: "MERN_STACK_EVENT_MESSAGE"}
+    )
+    .then(()=>{
+        console.log("Connected to database!")
+    })
+    .catch((err) => {
+        console.log("some error occured while connecting to database:",err);
+
+
+ });   
+
+};
