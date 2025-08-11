ff5bfc65a0983ba31d0cda08d2fd3fca0279ba29
Bhavya kumari <kumaribhavya425@gmail.com>
2025-08-11 16:19:37 +0530

    final commit

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -6,17 +6,10 @@ const About = () => {
       <h4>Events</h4>
       <h2>ABOUT</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet facilis
-        voluptas provident animi corrupti dolor, ipsum deleniti perferendis
-        veniam neque labore enim odio ratione. Ad commodi rem corporis soluta
-        sed reprehenderit a quasi nihil, odit aperiam exercitationem? Suscipit
-        rerum quibusdam inventore atque veritatis accusantium excepturi,
-        molestias optio perferendis aliquam harum, amet dolore! Animi
-        consequuntur officiis modi eius cumque reiciendis harum ipsum
-        praesentium numquam fugit? Consequatur eveniet recusandae harum hic. Eum
-        illo praesentium optio accusantium, hic quibusdam delectus voluptatem
-        qui a officiis dolorum minima cum illum culpa doloremque quia quidem aut
-        sed laudantium nam repudiandae sequi similique! Esse natus at quas?
+        Our Event Management Platform helps users discover, organize, and participate in events with ease. 
+        Built using the MERN stack, it offers a smooth and responsive experience for both organizers and attendees.
+        From concerts and conferences to workshops and meetups, we bring communities together to celebrate and learn. 
+        Join us to connect, explore, and create unforgettable experiences.
       </p>
     </section>
   );
